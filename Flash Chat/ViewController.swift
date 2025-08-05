3e84f6cbc78814875142be9f023dc835e6a7f2f9
vasiliev-evgeny <vasiliev.creativnoemilo99@gmail.com>
2025-08-05 17:59:48 +0300

    Initial Commit

---
+++ Flash Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Евгений Васильев on 05.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
