7adf1bbdb7baa54237080ce5862c34a882f42583
vasiliev-evgeny <vasiliev.creativnoemilo99@gmail.com>
2025-08-07 15:40:00 +0300

    All UI done

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -7,6 +7,7 @@
 import UIKit
 
 class ChatViewController : UIViewController {
+    
     enum Constants {
         
     }
@@ -18,14 +19,31 @@ class ChatViewController : UIViewController {
         field.textAlignment = .left
         field.placeholder = "Write your message..."
         field.layer.cornerRadius = 5
+        field.backgroundColor = .white
         return field
     }()
     
     let sendButton : UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "paperplane.fill"), for: .normal)
+        button.tintColor = .white
         return button
     }()
+    
+    let messageTableView : UITableView = {
+        let view = UITableView()
+        view.rowHeight = 50
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    //MARK: - Set Delegates
+    
+    func setDelegates() {
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        messageTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MessageCell")
+    }
     
     //MARK: - Lifecycle
     
@@ -33,12 +51,14 @@ class ChatViewController : UIViewController {
         super.viewDidLoad()
         setupViews()
         setConstraints()
+        setDelegates()
     }
     
     private func setupViews() {
         view.backgroundColor = .brandPurple
         view.addSubview(messageTextField)
         view.addSubview(sendButton)
+        view.addSubview(messageTableView)
     }
     
     //MARK: - setConstraints
@@ -46,18 +66,43 @@ class ChatViewController : UIViewController {
     private func setConstraints() {
         messageTextField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-        messageTextField.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -34),
-        messageTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        messageTextField.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: 20),
-        messageTextField.heightAnchor.constraint(equalTo: view.heightAnchor, constant: 50),
+            messageTextField.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -34),
+            messageTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            messageTextField.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: -20),
+            messageTextField.heightAnchor.constraint(equalToConstant: 50),
         ])
         
         sendButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             sendButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -34),
             sendButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            sendButton.heightAnchor.constraint(equalTo: view.heightAnchor, constant: 50),
+            sendButton.heightAnchor.constraint(equalToConstant: 50),
             sendButton.widthAnchor.constraint(equalToConstant: 50)
         ])
+        
+        messageTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            messageTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            messageTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            messageTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            messageTableView.bottomAnchor.constraint(equalTo: messageTextField.topAnchor, constant: -15)
+        ])
+    }
+}
+
+//MARK: - Extension TableView
+
+extension ChatViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+        cell.textLabel?.text = "ogo way"
+        cell.textLabel?.numberOfLines = 0
+        cell.backgroundColor = .clear
+        return cell
     }
 }
